9187bfa709d80e91e8cc7211660a43f8b28fbf4d
Natip85 <106977567+Natip85@users.noreply.github.com>
2022-06-21 14:52:25 +0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+var firstP = 'I really am afriad of JS';
+
+console.log(firstP);
+
+var secondP = 'loving JS';
+
+console.log(secondP);
+
+var thirdP = firstP.slice(0,12);
+console.log(thirdP);
+
+var fourthP = thirdP+secondP;
+console.log(fourthP);
